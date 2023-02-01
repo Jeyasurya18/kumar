ca3b18f21e7c9c33be84fac989afc7325ece389b
Jeyasurya <pkssuryasd718@gmail.com>
2023-02-01 16:11:32 +0530

    success

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+		System.out.println("Greens Omr is Chennai");
+
+	}
+	private void greensAdayar() {
+		System.out.println("Greens Adayar is Chennai");
+
+	}
+	private void greensTambaram() {
+		System.out.println("Greens Tambaram is Chennai");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("Greens Velacherry is Chennai");
+
+	}
+	private void greensAnnanagar() {
+	System.out.println("Greens Annanagar is Chennai");
+
+	}
+	public static void main(String[] args) {
+		GreensTech g= new GreensTech();
+				g.greensOmr();
+				g.greensAdayar();
+				g.greensTambaram();
+				g.greensVelacherry();
+				g.greensAnnanagar();
+				
+	}
+
+}
